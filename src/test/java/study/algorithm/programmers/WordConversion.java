5e6a5e62d79d53b7eb748a4365e72dc41dbc141b
kimhs <khsb2012@gmail.com>
2021-02-22 14:42:31 +0900

    1. 프로그래머스 단어변환 문제 리팩토링

--- src/test/java/study/algorithm/programmers/WordConversion.java
+++ src/test/java/study/algorithm/programmers/WordConversion.java
@@ -4,18 +4,14 @@ import java.util.Arrays;
 
 public class WordConversion {
 
-  private boolean[] visited;
-
   public int solution(String begin, String target, String[] words) {
-    visited = new boolean[words.length];
-
     if (!Arrays.asList(words).contains(target))
       return 0;
 
-    return findMinConvertCount(words, begin, target, 0, words.length);
+    return findMinConvertCount(words, new boolean[words.length], begin, target, 0, words.length);
   }
 
-  public int findMinConvertCount(String[] words, String standardWord, String target, int count, int minCount) {
+  public int findMinConvertCount(String[] words, boolean[] visited, String standardWord, String target, int count, int minCount) {
     if (standardWord.equals(target)) {
       return count;
     }
@@ -23,7 +19,7 @@ public class WordConversion {
     for (int i = 0; i < words.length; i++) {
       if (!visited[i] && isConvertable(standardWord, words[i])) {
         visited[i] = true;
-        int tmpCount = findMinConvertCount(words, words[i], target,count + 1, minCount);
+        int tmpCount = findMinConvertCount(words, visited, words[i], target,count + 1, minCount);
         visited[i] = false;
 
         minCount = Math.min(minCount, tmpCount);
