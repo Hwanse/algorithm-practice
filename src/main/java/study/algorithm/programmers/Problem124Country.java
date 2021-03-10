a2d6410110cb27465bfe71345c35e93e29ba6c51
kimhs <khsb2012@gmail.com>
2021-03-10 23:09:02 +0900

    1. 프로그래머스 124나라 풀이

--- src/main/java/study/algorithm/programmers/Problem124Country.java
+++ src/main/java/study/algorithm/programmers/Problem124Country.java
@@ -1,5 +1,6 @@
 package study.algorithm.programmers;
 
+// 규칙성을 찾아야함
 public class Problem124Country {
 
     private final int[] numbers = new int[] {4, 1, 2};
@@ -12,6 +13,9 @@ public class Problem124Country {
             int remainder = decimal % 3;
             decimal /= 3;
 
+            // 나머지가 0으로 나누어 떨어지는 순간 자리수가 변경이되며
+            // 해당 자리수 값이 예시대로 제대로 나올려면 -1 씩 연산되어야하는
+            // 규칙성이 있다.
             if (remainder == 0) {
                 decimal--;
             }
