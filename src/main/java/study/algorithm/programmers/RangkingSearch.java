66229ac30fd44c88eedb212a2a61a59d68fe7815
hwanse <khsb2012@gmail.com>
2021-04-04 23:20:32 +0900

    1. 프로그래머스 순위 검색 문제 풀이

---
+++ src/main/java/study/algorithm/programmers/RangkingSearch.java
@@ -0,0 +1,5 @@
+package study.algorithm.programmers;
+
+public class RangkingSearch {
+
+}
---
+++ src/test/java/study/algorithm/programmers/RangkingSearchTest.java
@@ -0,0 +1,14 @@
+package study.algorithm.programmers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RangkingSearchTest {
+
+    @Test
+    public void solution() throws Exception {
+    }
+
+
+}
