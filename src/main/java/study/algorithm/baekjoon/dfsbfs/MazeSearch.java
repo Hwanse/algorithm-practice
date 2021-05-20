ce323a10f9d6b0360e74bc64c8d518bf78e7529c
hwanse <khsb2012@gmail.com>
2021-05-20 20:04:51 +0900

    - 백준 미로찾기 문제 풀이

--- src/main/java/study/algorithm/baekjoon/dfsbfs/MazeSearch.java
+++ src/main/java/study/algorithm/baekjoon/dfsbfs/MazeSearch.java
@@ -3,18 +3,13 @@ package study.algorithm.baekjoon.dfsbfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MazeSearch {
 
     private static int[][] maze;
-    private static int[][] temp;
-
-    private static final int NOT_ROAD = 0;
-    private static final int ROAD = 1;
-    private static final int VISIT_ROAD = 2;
-    private static final int BLOCK_ROAD = 3;
-
+    private static int[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,34 +18,55 @@ public class MazeSearch {
         int n = Integer.parseInt(size[0]);
         int m = Integer.parseInt(size[1]);
         maze = new int[n][m];
-        temp = new int[n][m];
+        visited = new int[n][m];
+        
 
         for (int i = 0; i < n; i++) {
             String input = br.readLine();
             for (int j = 0; j < input.length(); j++) {
                 maze[i][j] = input.charAt(j) - '0';
-                temp[i][j] = input.charAt(j) - '0';
             }
         }
 
+        search(0, 0);
+        System.out.println(visited[n - 1][m - 1]);
 
-
-    }
-
-    private static void printArr(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(Arrays.toString(arr[i]));
-        }
     }
 
     private static void search(int x, int y) {
-        if (temp[x][y] == NOT_ROAD || temp[x][y] == BLOCK_ROAD) {
-            return;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        visited[x][y] = 1;
+
+        while (!queue.isEmpty()) {
+            int[] location = queue.poll();
+            int currentX = location[0];
+            int currentY = location[1];
+
+            for (Direction dir : Direction.values()) {
+                int nextX = currentX + dir.x;
+                int nextY = currentY + dir.y;
+
+                if (checkLocation(nextX, nextY)) {
+                    queue.offer(new int[] {nextX, nextY});
+                    visited[nextX][nextY] = visited[currentX][currentY] + 1;
+                }
+            }
         }
     }
 
     private static boolean isInnerOfRange(int x, int y) {
         return x >= 0 && y >= 0 && x < maze.length && y < maze[x].length;
+    }
+
+    private static boolean checkLocation(int x, int y) {
+        if (!isInnerOfRange(x, y)) {
+            return false;
+        }
+        if (maze[x][y] == 0 || visited[x][y] != 0) {
+            return false;
+        }
+        return true;
     }
 
     enum Direction {
